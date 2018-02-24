2e4eafedd7caa879729b588cbb0bb1c0b0562cf6
Philippe Lawson <philippe.lawson@lawson-it.com>
2018-02-24 15:47:45 +0100

    Fixed issue with worker hostnames not being resolved

--- kubernetes-storm/docker/worker/start.sh
+++ kubernetes-storm/docker/worker/start.sh
@@ -2,4 +2,4 @@
 
 /configure.sh ${ZK_CS_SERVICE_HOST:-$1} ${NIMBUS_SERVICE_HOST:-$2}
 
-exec bin/storm supervisor
+exec bin/storm supervisor -c storm.local.hostname=$(hostname -i | awk '{print $1;}')
