81b599e8e690f5fdc8c87bceeb026b88a81debfc
Philippe Lawson <philippe.lawson@lawson-it.com>
2018-07-28 10:39:32 +0200

    Added non k8s VMs

---
+++ virtual-machines/bin/createvms_no_k8s.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo /bin/bash ./mkvm.sh --user plawson --name kube-node07 --mac 080027c567c1
+sudo /bin/bash ./mkvm.sh --user plawson --name kube-node08 --mac 080027a3361e
+sudo /bin/bash ./mkvm.sh --user plawson --name kube-node09 --mac 08002735540f
+sudo /bin/bash ./mkvm.sh --user plawson --name kube-node10 --mac 08002773574b
+sudo /bin/bash ./mkvm.sh --user plawson --name kube-node11 --mac 080027f4c33d
---
+++ virtual-machines/bin/start_no_k8s.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./startvms.sh kube-node07
+./startvms.sh kube-node08
+./startvms.sh kube-node09
+./startvms.sh kube-node10
+./startvms.sh kube-node11
